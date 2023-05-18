2a4c012944039179774fc7503d6db6f021a2c11a
baubyte <53358294+baubyte@users.noreply.github.com>
2023-05-17 22:16:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -23,8 +23,8 @@ const APP_SHELL_INMUTABLE = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.1/css/all.min.css',
-    '/css/animate.css',
-    '/js/libs/jquery.js'
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
 self.addEventListener('install', event => {
